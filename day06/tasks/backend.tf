a7cf66747507d199ca16dbac631595e4b55e549e
ADITYA MUTHYALA <aditya@ADITYAs-MacBook-Air-2.local>
2025-12-08 12:52:36 +0530

    task

---
+++ day06/tasks/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+
+
+  backend "s3" {
+    bucket         = "my-buck"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
